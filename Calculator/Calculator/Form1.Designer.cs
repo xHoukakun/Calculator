7fa0bf925f1e502b8284f8e317010989d3aad2b3
xHoukakun <muellersjannek@yahoo.de>
2017-03-15 08:59:34 +0100

    Zahlen Eingegeben

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -63,6 +63,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -72,6 +73,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -81,6 +83,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -90,6 +93,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -99,6 +103,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -108,6 +113,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -117,6 +123,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -126,6 +133,7 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -135,6 +143,7 @@
             this.button9.TabIndex = 8;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button0
             // 
@@ -144,6 +153,7 @@
             this.button0.TabIndex = 9;
             this.button0.Text = "0";
             this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
             // 
             // buttonKomma
             // 
@@ -154,6 +164,7 @@
             this.buttonKomma.TabIndex = 11;
             this.buttonKomma.Text = ",";
             this.buttonKomma.UseVisualStyleBackColor = true;
+            this.buttonKomma.Click += new System.EventHandler(this.buttonKomma_Click);
             // 
             // buttonLöschen
             // 
@@ -181,6 +192,7 @@
             this.buttonPlus.TabIndex = 14;
             this.buttonPlus.Text = "+";
             this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
             // 
             // buttonReverse
             // 
@@ -199,6 +211,7 @@
             this.buttonMinus.TabIndex = 16;
             this.buttonMinus.Text = "-";
             this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
             // 
             // buttonMal
             // 
@@ -208,6 +221,7 @@
             this.buttonMal.TabIndex = 17;
             this.buttonMal.Text = "*";
             this.buttonMal.UseVisualStyleBackColor = true;
+            this.buttonMal.Click += new System.EventHandler(this.buttonMal_Click);
             // 
             // buttonGeteilt
             // 
@@ -217,6 +231,7 @@
             this.buttonGeteilt.TabIndex = 18;
             this.buttonGeteilt.Text = "/";
             this.buttonGeteilt.UseVisualStyleBackColor = true;
+            this.buttonGeteilt.Click += new System.EventHandler(this.buttonGeteilt_Click);
             // 
             // textBoxErsteZahl
             // 
@@ -227,6 +242,7 @@
             this.textBoxErsteZahl.Name = "textBoxErsteZahl";
             this.textBoxErsteZahl.Size = new System.Drawing.Size(200, 20);
             this.textBoxErsteZahl.TabIndex = 19;
+            this.textBoxErsteZahl.TextChanged += new System.EventHandler(this.textBoxErsteZahl_TextChanged);
             // 
             // textBoxZweiteZahl
             // 
--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -12,9 +12,232 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        private bool bRechenoperation = false;
+        private bool bDezimal = false;
+        private bool bErsteStelle = false;
+        private bool bEineRechenoperation = false;
+        private int iNameRechenoperation = 0;       //0=Keine 1=Addieren 2=Multiplizieren 3=Minus 4=Geteilt
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            bErsteStelle = true;
+            if (bRechenoperation==false)
+            {
+                textBoxErsteZahl.AppendText("0");
+
+            }
+            if (bRechenoperation==true)
+            {
+                textBoxZweiteZahl.AppendText("0");
+            }
+
+        }
+
+        private void textBoxErsteZahl_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bErsteStelle = true;
+            if (bRechenoperation == false)
+            {
+                textBoxErsteZahl.AppendText("1");
+
+            }
+            if (bRechenoperation == true)
+            {
+                textBoxZweiteZahl.AppendText("1");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bErsteStelle = true;
+            if (bRechenoperation == false)
+            {
+                textBoxErsteZahl.AppendText("2");
+
+            }
+            if (bRechenoperation == true)
+            {
+                textBoxZweiteZahl.AppendText("2");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bErsteStelle = true;
+            if (bRechenoperation == false)
+            {
+                textBoxErsteZahl.AppendText("3");
+
+            }
+            if (bRechenoperation == true)
+            {
+                textBoxZweiteZahl.AppendText("3");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bErsteStelle = true;
+            if (bRechenoperation == false)
+            {
+                textBoxErsteZahl.AppendText("4");
+
+            }
+            if (bRechenoperation == true)
+            {
+                textBoxZweiteZahl.AppendText("4");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bErsteStelle = true;
+            if (bRechenoperation == false)
+            {
+                textBoxErsteZahl.AppendText("5");
+
+            }
+            if (bRechenoperation == true)
+            {
+                textBoxZweiteZahl.AppendText("5");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            bErsteStelle = true;
+            if (bRechenoperation == false)
+            {
+                textBoxErsteZahl.AppendText("6");
+
+            }
+            if (bRechenoperation == true)
+            {
+                textBoxZweiteZahl.AppendText("6");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            bErsteStelle = true;
+            if (bRechenoperation == false)
+            {
+                textBoxErsteZahl.AppendText("7");
+
+            }
+            if (bRechenoperation == true)
+            {
+                textBoxZweiteZahl.AppendText("7");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            bErsteStelle = true;
+            if (bRechenoperation == false)
+            {
+                textBoxErsteZahl.AppendText("8");
+
+            }
+            if (bRechenoperation == true)
+            {
+                textBoxZweiteZahl.AppendText("8");
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            bErsteStelle = true;
+            if (bRechenoperation == false)
+            {
+                textBoxErsteZahl.AppendText("9");
+
+            }
+            if (bRechenoperation == true)
+            {
+                textBoxZweiteZahl.AppendText("9");
+            }
+        }
+
+        private void buttonKomma_Click(object sender, EventArgs e)
+        {
+            if (bErsteStelle == true)
+            {
+                if (bDezimal == false)
+                {
+                    if (bRechenoperation == false)
+                    {
+                        textBoxErsteZahl.AppendText(",");
+
+                    }
+                    if (bRechenoperation == true)
+                    {
+                        textBoxZweiteZahl.AppendText(",");
+                    }
+                    bDezimal = true;
+                }
+            }
+        }
+
+        private void buttonGeteilt_Click(object sender, EventArgs e) //0=Keine 1=Addieren 2=Multiplizieren 3=Minus 4=Geteilt
+        {
+            if (bEineRechenoperation == false && bErsteStelle==true)
+            {
+                iNameRechenoperation=4;
+                bEineRechenoperation = true;
+                bErsteStelle = false;
+                bDezimal = false;
+                bRechenoperation = true;
+            }
+
+        }
+
+        private void buttonMal_Click(object sender, EventArgs e)
+        {
+            if (bEineRechenoperation == false && bErsteStelle == true)
+            {
+                iNameRechenoperation = 2;
+                bEineRechenoperation = true;
+                bErsteStelle = false;
+                bDezimal = false;
+                bRechenoperation = true;
+            }
+
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            if (bEineRechenoperation == false && bErsteStelle == true)
+            {
+                iNameRechenoperation = 3;
+                bEineRechenoperation = true;
+                bErsteStelle = false;
+                bDezimal = false;
+                bRechenoperation = true;
+            }
+
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            if (bEineRechenoperation == false && bErsteStelle == true)
+            {
+                iNameRechenoperation = 1;
+                bEineRechenoperation = true;
+                bErsteStelle = false;
+                bDezimal = false;
+                bRechenoperation = true;
+            }
+
         }
     }
 }
